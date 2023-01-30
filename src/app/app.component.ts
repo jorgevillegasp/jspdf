e7c43401f667c33db2dd0b2dbf99ed55a59c927f
Jorge Villegas <jorge.villegasp@hotmail.com>
2023-01-30 14:36:17 -0500

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,32 +46,10 @@ export class AppComponent {
       reference: '1-123-3336-4321',
     },
   ];
+
+  base64 = "";
   title = 'JsPDF';
-  /**
-   * Asignamos en “DATA” el contenido de la tabla 
-   * y el título “Lista de videojuegos…” para poder 
-   * usarlo en la generación del PDF como la 
-   * información que queremos mostrar.
-   */
-  
-  generatePDF() {
-    const doc = new jsPDF('p', 'mm', 'a4');
-    
-    // Encabezado
-    doc.setFontSize(20);
-    doc.text(`<h1>Encabezado</h1>`, 10, 20);
- 
-    // Cuerpo
-    doc.setFontSize(14);
-    doc.text(JSON.stringify(this.VIDEOGAMES), 10, 40);
- 
-    // Pie de página
-    doc.setFontSize(12);
-    doc.text('Pie de página', 10, 280);
-    
-    doc.save('data.pdf');
-  }
-  
+
   
   // tslint:disable-next-line:typedef
   descargarPDF() {
@@ -106,6 +84,7 @@ export class AppComponent {
       const pdfWidth = pdf.internal.pageSize.getWidth() - 2 * bufferX;
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
       //addImage (imageData, formato, x, y, ancho, alto, alias, compresión, rotación) 
+
       pdf.addImage(
         img, 
         'PNG', 
@@ -116,67 +95,57 @@ export class AppComponent {
         undefined, 
         'FAST'
       );
+
+      this.almacenamosElPDFenUnJson(img);
       
       return pdf;
       
     }).then((docResult) => {
-
-      this.convertiloEnBase64(docResult);
-      
+      //this.convertiloEnBase64(docResult);
       /** Nombre del documento que se descargara */
       docResult.save(`${ this.title +"_" + new Date().toISOString()}.pdf`);
+      
     });
 
     
   }
 
-  convertiloEnBase64(docResult:any){
-    /** Obtén el contenido binario del PDF con la función output de JsPDF: */
-      // ... agrega contenido al PDF
-      const pdfData = docResult.output('datauristring');
+  almacenamosElPDFenUnJson(img:any){
+    let objetoJSON = {
+      pdf: img,
+    };
 
-      //Convierte el contenido binario en una cadena base64:
-      const base64 = btoa(pdfData);
-
-      //Almacena el contenido base64 en un objeto JSON:
-      const json = { pdfData: base64 };
-
-      console.log("JSON = ",json);
-
-      this.decodificarUnBase64(json);
-      
-      
-
-
-      // Ahora puedes almacenar este objeto JSON en MongoDB 
-      // usando una operación de inserción. También puedes 
-      // enviar este objeto a una API para su almacenamiento 
-      // en la base de datos.
-      
-
-      // Cuando necesites recuperar el PDF desde la base de datos, 
-      // simplemente convierte el contenido base64 en una cadena 
-      // binaria y crea un nuevo objeto JsPDF con ese contenido.
-
-
-
+    this.decodificarUnBase64(objetoJSON);
   }
 
-  decodificarUnBase64(json:any){
-    //decodificando la base64
-    const pdfData = atob(json.pdfData);
-    console.log("Retorno = ",pdfData);
-    
 
+  decodificarUnBase64(Json:any){
+
+    const img = Json.pdf;
 
     const pdf = new jsPDF('p', 'pt', 'a4');
-    pdf.save('documentoDecodificado.pdf');
+    // Add image Canvas to PDF
+    /**Establecer un margen en funcion de X para el PDF  */
+    const bufferX = 15;
+    /**Establecer un margen en funcion de Y para el PDF  */
+    const bufferY = 15;
+    /** También se calcula el tamaño del PDF en función del tamaño de la imagen capturada. */
+    const imgProps = (pdf as any).getImageProperties(img);
+    const pdfWidth = pdf.internal.pageSize.getWidth() - 2 * bufferX;
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    //addImage (imageData, formato, x, y, ancho, alto, alias, compresión, rotación) 
 
-
-    // Or open the PDF file in a new tab
-    var blobUrl = URL.createObjectURL(json.pdfData);
-    window.open(blobUrl);
-   
+    pdf.addImage(
+      img, 
+      'PNG', 
+      bufferX, 
+      bufferY, 
+      pdfWidth, 
+      pdfHeight, 
+      undefined, 
+      'FAST'
+    );
+      pdf.save("download.pdf"); 
   }
 
 
